b790371d5241b143cc3d82c5f9c44d54c47de0d5
Dydier Ripe <82624734+DydierRipe@users.noreply.github.com>
2023-07-25 17:33:13 -0500

    Add files via upload

---
+++ src/front/js/index.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const promotions = document.querySelector(".promotions");
+const searchBar = document.querySelector("#search-bar");
+
+document.addEventListener("keyup", e => {
+    if (e.key == "Enter" && searchBar.value.length != 0 && searchBar === document.activeElement) {
+        window.location.replace("/search" + "#" + searchBar.value);
+    }
+});
+
+const userString = localStorage.getItem("user"); // user info
+
+for (let i = 0; i < promotions.children.length; i++) {
+    promotions.children[i].addEventListener("mouseover", () => {
+        promotions.children[1].style = "transform: scale(100%);"
+    });
+    
+    promotions.children[i].addEventListener("mouseout", () => {
+        promotions.children[1].style = "transform: scale(120%);"
+    });
+}
+
+if (userString === null) {
+    const pus = document.querySelector(".logged > p");
+    pus.innerHTML = "Not logged";
+} else {
+    const user = JSON.parse(userString);
+    const pus = document.querySelector(".logged > p > span");
+    pus.innerHTML = `user: ${user.username}<br>email: ${user.email}`;
+
+    const getOut = document.querySelector(".get-out");
+
+    getOut.addEventListener("click", () => {
+        localStorage.removeItem("user");
+        window.location.replace("/login"); 
+    });
+}
