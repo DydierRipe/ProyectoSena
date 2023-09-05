a85b0e89e81901c55cd148586f58e8adb3791b62
Dydier Ripe <82624734+DydierRipe@users.noreply.github.com>
2023-09-05 02:03:18 -0500

    Add files via upload

---
+++ src/front/js/product.js
@@ -0,0 +1,108 @@
+const image = document.querySelector(".image");
+const prName = document.querySelector(".product-name");
+const price = document.querySelector(".price");
+const description = document.querySelector(".description");
+const filter = document.querySelector(".filter-container");
+const buyButton = document.querySelector(".buy-button");
+const gallery = document.querySelector(".product-gallery")
+
+const userString = localStorage.getItem("user"); // user info
+
+const numberWithCommas = x => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const getData = (updateGallery=true) => {
+    filter.innerHTML = '';
+    fetch("/product", { 
+        method: "POST",
+        body: JSON.stringify({"reason": "getProductData", "id": window.location.hash.replace('#', '')}),
+        headers : { "Content-type" : "application/json" }
+    }).then(res => res.json())
+    .then(res => {
+        if (res[0] == "FE") {
+            console.log("error lol");
+        } else {
+            let resp = res[1];
+            res = res[0];
+
+            image.src = "media/productImages/" + window.location.hash.replace('#', '') + res[0].extension;
+            
+            document.title = res[0].name;
+
+            if (res[0].owner == 'no-owner') prName.textContent = res[0].name;
+            else {
+                prName.textContent = res[0].name;
+                const fr = document.createDocumentFragment();
+                const soldMes = document.createElement("span");
+                soldMes.textContent = res[0].owner == JSON.parse(userString).email? " Reserved by you" : " Reserved";
+                soldMes.classList.add("sold");
+                fr.appendChild(soldMes);
+                prName.appendChild(fr);
+            }
+
+            price.textContent = '$' + numberWithCommas(res[0].price);
+            description.textContent = res[0].description;
+            res[0].filters = res[0].filters.split(',');
+            const fr = document.createDocumentFragment();
+            res[0].filters.forEach(e => {
+                const tag = document.createElement("div");
+                tag.classList.add("filter-box");
+                tag.textContent = e;
+
+                fr.appendChild(tag);
+            });
+            filter.appendChild(fr);
+
+            let i = 0;
+            if (updateGallery) {
+                for (let element of gallery.children) {
+                    const fra = document.createDocumentFragment();
+                    const tit = document.createElement("h3");
+                    const img = document.createElement("img");
+
+                    element.setAttribute("data-id", resp[i].id);
+                    tit.textContent = resp[i].name;
+                    img.src = "media/productImages/" + resp[i].id + resp[i].extension;
+                    img.alt = "product image";
+                    img.classList.add("product-image");
+
+                    fra.appendChild(tit);
+                    fra.appendChild(img);
+                    element.appendChild(fra);
+
+                    element.addEventListener("click", () => window.location.replace("/product#" + element.getAttribute("data-id")));
+                    i++;
+                }
+            }
+
+            if (userString !== null && res[0].owner == 'no-owner') {
+                buyButton.addEventListener("click", () => {
+                    fetch("/product", { 
+                        method: "POST",
+                        body: JSON.stringify({"reason": "sellToUser", "id": window.location.hash.replace('#', ''), "user": JSON.parse(userString).email}),
+                        headers : { "Content-type" : "application/json" }
+                    }).then(res => res.json())
+                    .then(res => {
+                        if (res[0] == 'Sold') {
+                            getData();
+                        }
+                    });
+                });
+            }
+        }
+    });
+}
+
+window.addEventListener("hashchange", () => getData(false));
+
+getData();
+
+if (userString === null) {
+    const pus = document.querySelector(".logged > p");
+    pus.innerHTML = "Not logged";
+} else {
+    const user = JSON.parse(userString);
+    const pus = document.querySelector(".logged > p > span");
+    pus.innerHTML = `user: ${user.username}<br>email: ${user.email}`;
+}
---
+++ src/index.js
@@ -0,0 +1,136 @@
+"use strict"
+
+const path = require("node:path"); // use an especific path to save time
+const express = require("express"); // express library (nothing would work without this)
+const fs = require('fs');
+const multer = require("multer");
+
+const index = require("./back js/pages/index.js");
+const cart = require("./back js/pages/cart.js");
+const login = require("./back js/pages/login.js");
+const signup = require("./back js/pages/signup.js");
+const search = require("./back js/pages/search.js");
+const users = require("./back js/pages/users.js");
+const product = require("./back js/pages/product.js");
+const database = require("./back js/libraries/connect.js");
+
+database.connect(e => {if (e) throw e;});
+const app = express();
+
+app.use(express.json()); // can interpret json
+app.use(express.static(__dirname + "/front")); // front end files searching ease
+app.use("/media", express.static(__dirname + '/sources'));
+
+const upload = multer();
+
+app.listen(3000, () => { 
+    console.log("server is working");
+});
+
+app.get("", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/index.html"))
+});
+
+app.post("", async (req, res) => {
+    let resMes = await index.main();
+    
+    res.send(resMes);
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/login.html"));
+});
+
+/*
+    response Types: 
+        N: the user doesn't exists, try with a registered user or sign up with your account
+        NC: the user exists and the password is correct, can log in
+        NI: the user exists but the password is incorrect, try with other password
+*/
+
+app.post("/login", async (req, res) => {
+    let resMes = await login.main(req);
+    
+    res.send(resMes);
+});
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/signup.html"));
+});
+
+/*
+    response Types: 
+        E: the email is already registered
+        FE: file error, the email can be registered (not exist in the database) but there was an error in the database writing system, try again later
+        EmS: email sended
+*/ 
+
+app.post("/signup", async (req, res) => {
+    let resMes = await signup.main(req.body);
+
+    res.send(resMes);
+});
+
+app.get("/search", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/search.html"));
+});
+
+app.post("/search", async (req, res) => {
+    let resMes = await search.main(req.body);
+
+    res.send(resMes);
+});
+
+app.get("/usersadmin", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/users.html"));
+});
+
+app.post("/usersadmin", async (req, res) => {
+    let resMes = await users.main(req.body);
+
+    res.send(resMes);
+});
+
+app.get("/creating", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/addProduct.html"));
+});
+
+app.post("/creating", upload.any(), async (rem, res) => {
+    let req = JSON.parse(rem.body.product);
+
+    database.query(`insert into products(name, price, description, instrumenttype, filters, seller)
+    values ("${req.name}", ${req.price}, "${req.description}", ${req.instrumentType}, "${req.filters}", "${req.seller}")`, async (e, resp) => {
+        if (e) throw e;
+
+        let origName = rem.files[0].originalname;
+        const savePath = path.join(__dirname, 'sources/productImages', resp.insertId + "." + origName.split(".").pop());
+
+        fs.writeFile(savePath, rem.files[0].buffer, (err) => {
+            if (err) {
+                res.send(["FE"]);
+                throw err;
+            }
+            res.send([resp.insertId]);
+        });
+    }); 
+});
+        
+app.get("/product", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/product.html"));
+});
+
+app.post("/product", async (req, res) => {
+    let resMes = await product.main(req.body);
+
+    res.send(resMes);
+});
+
+app.get("/reserved", (req, res) => {
+    res.sendFile(path.join(__dirname + "/front/html/cart.html"))
+});
+
+app.post("/reserved", async (req, res) => {
+    let resMes = await cart.main(req.body);
+    
+    res.send(resMes);
+});
